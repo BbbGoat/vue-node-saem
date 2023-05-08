2f3c2991ce3692e517177ebc83abcd68919ca158
BbbGoat <oomi9421@gmail.com>
2023-05-08 12:22:02 +0900

    ss

---
+++ my-vue-city-app/src/components/TopComp.vue
@@ -0,0 +1,60 @@
+<template>
+  
+  <header>
+    <ul class="gnb">
+        <li>
+            <a href="#" v-on:click="chgData('서울')">서울</a>
+        </li>
+        <li>
+            <a href="#" v-on:click="chgData('부산')">부산</a>
+        </li>
+        <li>
+            <a href="#" v-on:click="chgData('제주')">제주</a>
+        </li>
+    </ul>
+  </header>
+    
+</template>
+
+<script>
+export default {
+  name: 'TopArea',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+header {
+    padding: 15px;
+    border: 2px solid #ccc;
+}
+header ul {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    display: flex;
+    justify-content: center;
+    gap: 0 15px;
+}
+header a {
+    font-size: 30px;
+    color: #222;
+    text-decoration: none;
+}
+header a.on {
+    font-size: 30px;
+    color: orangered;
+    text-decoration: overline;
+    text-decoration-style: wavy;
+}
+header a:hover {
+    color: orangered;
+    text-decoration: overline;
+    text-decoration-style: wavy;
+}
+</style>
---
+++ my-vue-city-app/src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App'
+
+
+/* 뷰인스턴스 생성하기! */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
